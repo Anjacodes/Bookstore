27e7b2029759b514be69f68ebdc4f9ed95299b5d
Anja Schmidt <anja_schmidt7@posteo.de>
2022-01-17 10:35:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-Linters-template
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My React App</h2>
+    </div>
+  );
+}
+
+export default App;
