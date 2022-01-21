95a06f40ffdc63c7d44ac26636275eab9dc50462
Anjacodes <anja_schmidt7@posteo.de>
2022-01-21 06:28:47 -0600

    Implement Suggested Change From PR

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
